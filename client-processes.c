e86d4c851208afef0f7c3eff23e7ba17f4458bf1
Lukas <lukasborges@gmail.com>
2015-12-29 22:52:43 -0500

    Client using Processes, fork() instead of multithreads

--- client-processes.c
+++ client-processes.c
@@ -48,7 +48,8 @@ int main(int argc, char *argv[])
     int rv;
     char s[INET6_ADDRSTRLEN];
 
-    if (argc != 2) {
+    if (argc != 2)
+    {
         fprintf(stderr,"usage: client hostname\n");
         exit(1);
     }
@@ -57,40 +58,43 @@ int main(int argc, char *argv[])
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
 
-    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0)
+    {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
 
     // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
+    for(p = servinfo; p != NULL; p = p->ai_next)
+    {
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
+             p->ai_protocol)) == -1)
+        {
             perror("client: socket");
             continue;
         }
 
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+        {
             close(sockfd);
             perror("client: connect");
             continue;
         }
-
         break;
     }
 
-    if (p == NULL) {
+    if (p == NULL)
+    {
         fprintf(stderr, "client: failed to connect\n");
         return 2;
     }
 
     inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-            s, sizeof s);
+              s, sizeof s);
     printf("client: connecting to %s\n", s);
 
     freeaddrinfo(servinfo); // all done with this structure
     
-    //****EDIT STARTS HERE    
     puts("Nickname:");
     memset(&nickName, sizeof(nickName), 0);
     memset(&message, sizeof(message), 0); //clean message buffer
@@ -98,61 +102,84 @@ int main(int argc, char *argv[])
     //puts(message);
     
     //create new thread to keep receiving messages:
-    pthread_t recv_thread;
-    //new_sockfd = malloc(sizeof(int));
-    //new_sockfd = sockfd;
+    //pthread_t recv_thread;
+   
+    //create a new fork to keep receiving messages:
+    int iStackVariable = 20;
     
-    if( pthread_create(&recv_thread, NULL, receive_handler, (void*)sockfd) < 0)
-    { //(void*) sockfd
-        perror("could not create thread");
-        return 1;
-    }    
-    puts("Synchronous receive handler assigned");
-    
-    //send message to server:
-    puts("Connected\n");
-    puts("[Type '/quit' to quit]");
-    while(strcmp(sBuf,"/quit") != 0)
+    pid_t pID = fork(); //pass sockfd to fork
+   
+    if(pID == 0)	//child
     {
-        memset(&sBuf, sizeof(sBuf), 0); //clean sendBuffer
-        fgets(sBuf, 100, stdin); //gets(message);
-        int count = 0;
-        while(count < strlen(nickName))
+        int* sFd = (int*) sockfd;
+        char buffer[MAXDATASIZE];
+        int nBytes;
+
+        for(;;)
         {
-            message[count] = nickName[count];
-            count++;
-        }
-        count--;
-        message[count] = ':';
-        count++;
-        for(int i = 0; i < strlen(sBuf); i++)
-        {
-            message[count] = sBuf[i];
-            count++;
-        }
-        message[count] = '\0';
-        //puts(message);
-        //Send some data
-        //if(send(sockfd, sBuf , strlen(sBuf) , 0) < 0)
-        if(send(sockfd, message, strlen(message), 0) < 0)
-        {
-            puts("Send failed");
-            return 1;
-        }
-        memset(&sBuf, sizeof(sBuf), 0);
-        
-        /* receive message from client: 
-         * we move this to a thread in order to get synchronous recv()s.
-         */
+            if ((nBytes = recv(sFd, buffer, MAXDATASIZE-1, 0)) == -1)
+            {
+                perror("recv");
+                exit(1);
+            }
+            else
+                buffer[nBytes] = '\0';
+
+        printf("%s", buffer);
+        }    
     }
+    else if(pID < 0)
+    {
+        perror("failed to fork");
+        exit(1);
+    }
+    else
+    {
+	//code executed by parent only
+	puts("Connected\n");
+	puts("[Type '/quit' to quit]");
+	while(strcmp(sBuf,"/quit") != 0)
+	{
+            memset(&sBuf, sizeof(sBuf), 0); //clean sendBuffer
+            fgets(sBuf, 100, stdin); //gets(message);
+            int count = 0;
+            while(count < strlen(nickName))
+            {
+                message[count] = nickName[count];
+                count++;
+            }
+            count--;
+            message[count] = ':';
+            count++;
+            for(int i = 0; i < strlen(sBuf); i++)
+            {
+                message[count] = sBuf[i];
+                count++;
+            }
+            message[count] = '\0';
+            //puts(message);
+            //Send some data
+            //if(send(sockfd, sBuf , strlen(sBuf) , 0) < 0)
+            if(send(sockfd, message, strlen(message), 0) < 0)
+            {
+                puts("Send failed");
+                return 1;
+            }
+            memset(&sBuf, sizeof(sBuf), 0);
+            /* receive message from client: 
+             * we move this to a thread in order to get synchronous recv()s.
+             */
+	}
+    }//end code executed by parent only
     
+    //code executed by parent and child  
     //puts("Closing socket connection");
-    pthread_join(recv_thread , NULL);
+   // pthread_join(recv_thread , NULL);
     close(sockfd);
 
     return 0;
 }
-
+/*
 //thread function
 void *receive_handler(void *sock_fd)
 {
@@ -171,4 +198,4 @@ void *receive_handler(void *sock_fd)
             buffer[nBytes] = '\0';
         printf("%s", buffer);
     }
-}
+}*/
